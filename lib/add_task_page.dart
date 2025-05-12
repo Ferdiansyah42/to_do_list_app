7809832945669c5319f4d0aa5e3737c0d4421c85
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-05-12 23:39:36 +0700

    pembuatan class di add_tack

---
+++ lib/add_task_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'task_model.dart';
+
+class AddTaskPage extends StatefulWidget {
+  const AddTaskPage({super.key});
+
+  @override
+  State<AddTaskPage> createState() => _AddTaskPageState();
+}
