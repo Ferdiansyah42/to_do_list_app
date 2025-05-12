ee365b3a17d6522fc35bfa261696d4f0ce0130f1
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-05-12 23:41:19 +0700

    class AddTaskPageState

--- lib/add_task_page.dart
+++ lib/add_task_page.dart
@@ -7,3 +7,20 @@ class AddTaskPage extends StatefulWidget {
   @override
   State<AddTaskPage> createState() => _AddTaskPageState();
 }
+
+class _AddTaskPageState extends State<AddTaskPage> {
+  final titleController = TextEditingController();
+  final noteController = TextEditingController();
+  DateTime? selectedDate;
+
+  void _submit() {
+    if (titleController.text.isEmpty || selectedDate == null) return;
+
+    final task = Task(
+      title: titleController.text.trim(),
+      note: noteController.text.trim(),
+      schedule: selectedDate!,
+    );
+    Navigator.pop(context, task);
+  }
+}
