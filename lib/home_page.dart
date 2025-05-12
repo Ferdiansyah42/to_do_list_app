df956df456ec504a9984a810382438ccf70085f2
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-05-12 18:02:54 +0700

    pembuatan class home page

---
+++ lib/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  final VoidCallback onToggleTheme;
+  const HomePage({super.key, required this.onToggleTheme});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
