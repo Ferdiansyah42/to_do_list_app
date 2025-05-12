eccbc2f0c43b5c70611247125fd139438ed9ab52
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-05-12 18:51:42 +0700

    perapihan

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -11,8 +11,12 @@ class AboutPage extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(24.0),
           child: Text(
-            'Butuh catat tugas biar gak lupa? Ini dia solusinya!\n\n'
-            '🔥 Fitur keren:\n- Pindah halaman semudah swipe\n- Tema gelap buat kerja malam\n- Font kece\n- Tampilan modern'
+            'Buat catat tugas biar gak lupa? Ini dia solusinya!\n\n'
+            '🔥 Fitur keren:\n'
+            '- Pindah halaman semudah swipe\n'
+            '- Tema gelap buat kerja malam\n'
+            '- Font kece\n'
+            '- Tampilan modern',
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 16),
           ),
