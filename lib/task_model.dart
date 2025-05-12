469378873a9010ebc8a90092fbadcc9910f946bb
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-05-12 23:58:47 +0700

    task model

---
+++ lib/task_model.dart
@@ -0,0 +1,11 @@
+class Task {
+  final String title;
+  final String note;
+  final DateTime schedule;
+
+  Task({
+    required this.title,
+    required this.note,
+    required this.schedule,
+  });
+}
