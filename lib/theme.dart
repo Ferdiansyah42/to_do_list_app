582b676c5a04acce6d522a5f47f0eb189ade187d
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-05-12 19:16:49 +0700

    perbaikan

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,15 +3,19 @@ import 'package:flutter/material.dart';
 const defaultFont = 'Poppins';
 
 final ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.teal,
+    brightness: Brightness.light, // <- tambahkan ini
+  ),
   useMaterial3: true,
   fontFamily: defaultFont,
 );
 
 final ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.deepPurple,
+    brightness: Brightness.dark, // <- tambahkan ini
+  ),
   useMaterial3: true,
   fontFamily: defaultFont,
 );
